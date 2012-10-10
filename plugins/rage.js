c3ef35fea0b25aa3b3f49a19b012da318b30b56b
Butt Cactus <gr33nmous3@gmail.com>
2012-10-10 01:11:11 +0100

    wtf, i can't merge

--- plugins/rage.js
+++ plugins/rage.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-/*
- * @Plugin        rage
- * @Description   Capitalizes everything.
- *
- * @Author        Emn1ty (Jonathan Ardis)
- *
- */
- 
-Plugin = exports.Plugin = function (irc) {
-  irc.addTrigger('rage', this.rage);
-};
-
-Plugin.prototype.rage = function (irc, channel, nick, params, message, raw) {
-  var RAGE_MESSAGE = '',
-      symbols = ' %$#&@*!';;
-  if (params[0] == 'MOAR') {
-    for(var i=0; i <= 150; i++) {
-      RAGE_MESSAGE += symbols.charAt(Math.floor(Math.random() * symbols.length));
-    }
-  }
-  else {
-    RAGE_MESSAGE += params.join(' ').toUpperCase();
-  }
-  irc.send(channel, nick + ' RAGES!: ' + RAGE_MESSAGE + '!!!!!');
-=======
 /*
  * @Plugin        RAGE
  * @Description   Capitalizes everything.
@@ -72,5 +46,4 @@ Plugin.prototype.RAGE = function (msg) {
 
 
   irc.send(chanObj && chanObj.name || user, message_return);
->>>>>>> Emn1ty/master
 };
