a217354f9052750cfb334d00dee5c0d5303fce40
Draceros <gr33nmous3@gmail.com>
2013-02-03 23:03:29 +0000

    Added JSON file for memes and more functions, easy work

--- plugins/memepics.js
+++ plugins/memepics.js
@@ -7,48 +7,70 @@
  *
  */
 
-Plugin = exports.Plugin = function (irc) {
-  irc.addTrigger('brian', this.brian);
-};
-Plugin.prototype.brian = function (irc, channel, user, params, message) {
-  var generatorID;
+var memes = require("./memepics.json");
 
+Plugin = exports.Plugin = function (irc) {
+  for(var meme in memes) {
+    console.log("loading meme: "+meme);
+    irc.addTrigger(meme, function(i,c,u,p,m) {
+      var meme = message.split(" ")[0].replace(i.command, "");
+      this.meme(i,c,u,p,m,memes[meme]);
+    });
+  }
+
+  irc.addTrigger('meme', this.memeSwitch);
+};
+
+Plugin.prototype.memeSwitch = function(irc, channel, user, params, message) {
+  if(!params.length) {
+    return irc.send("Usage: "+irc.command+"meme "+Object.keys(memes).join("|")+' "line1" "line2", you can also use .[meme-name] "line1" "line2"');
+  }
+
+  var meme = params.shift();
+  if(memes[meme] !== undefined) return this.meme(irc, channel, user, params, message, memes[meme]);
+  else return irc.send("Meme "+meme+" not found");
+}
+
+Plugin.prototype.getLines = function(params) {
+  var msg1 = '';
+  var msg2 = '';
+  if(params[0][0] == '"') {
+    var msgNum = 1;
+    while(params.length) {
+      var p = params.shift();
+      var msgNumAfter = false;
+      if(p[0] == '"') {
+        p = p.substr(1);
+      } else if(p[p.length-1] == '"') {
+        p = p.substr(0, p.length-1);
+        msgNumAfter = true;
+      }
+
+      if(msgNum == 1) msg1 += p + " ";
+      else msg2 += p + " ";
+
+      if(msgNumAfter) msgNum++;
+    }
+  } else {
+    params.forEach(function(el, inx){
+      if(inx <= 4)
+        msg1 += el + ' ';
+      else
+        msg2 += el + ' ';
+    });
+  }
+
+  return [msg1.trim(),msg2.trim()]
+}
+
+Plugin.prototype.meme = function (irc, channel, user, params, message, generatorID) {
   if(params < 1)
     irc.send(channel, user + ', Attemps to bad luck brian someone. Forgets to add message.');
   else
   {
-    var msg1 = '';
-    var msg2 = '';
-    if(params[0][0] == '"') {
-      var msgNum = 1;
-      while(params.length) {
-        var p = params.shift();
-        var msgNumAfter = false;
-        if(p[0] == '"') {
-          p = p.substr(1);
-        } else if(p[p.length-1] == '"') {
-          p = p.substr(0, p.length-1);
-          msgNumAfter = true;
-        }
+    var msgs = this.getLines(params);
 
-        if(msgNum == 1) msg1 += p + " ";
-        else msg2 += p + " ";
-
-        if(msgNumAfter) msgNum++;
-      }
-    } else {
-      params.forEach(function(el, inx){
-        if(inx <= 4)
-          msg1 += el + ' ';
-        else
-          msg2 += el + ' ';
-      });
-    }
-
-    msg1 = msg1.trim();
-    msg2 = msg2.trim();
-
-    var url = 'http://version1.api.memegenerator.net/Instance_Create?username=w3bt3chirc&password=W3bT3ch1Rc507&languageCode=en&generatorID=740857&imageID=3459374&text0=' + msg1 + '&text1=' + msg2;
+    var url = 'http://version1.api.memegenerator.net/Instance_Create?username=w3bt3chirc&password=W3bT3ch1Rc507&languageCode=en&generatorID='+generatorID[0]+'&imageID='+generatorID[1]+'&text0=' + msgs[0] + '&text1=' + msgs[1];
 
     var http = require('http');
     var request = http.get(url, function(res)
