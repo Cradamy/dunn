a423a0dbd9c63481534ac14370f6953f37de203b
Draceros <gr33nmous3@gmail.com>
2013-02-03 22:39:59 +0000

    Added quotation marks to memepics.js for longer strings

--- plugins/memepics.js
+++ plugins/memepics.js
@@ -3,13 +3,13 @@
  * @Description   Serving up your favorite memes.
  * @Trigger       .brian
  * @Author        cgcardona
+ * @Contributor   polar-bear 
  *
  */
 
 Plugin = exports.Plugin = function (irc) {
   irc.addTrigger('brian', this.brian);
 };
-
 Plugin.prototype.brian = function (irc, channel, user, params, message) {
   var generatorID;
 
@@ -19,12 +19,35 @@ Plugin.prototype.brian = function (irc, channel, user, params, message) {
   {
     var msg1 = '';
     var msg2 = '';
-    params.forEach(function(el, inx){
-      if(inx <= 4)
-        msg1 += el + ' ';
-      else
-        msg2 += el + ' ';
-    });
+    if(params[0][0] == '"') {
+      var msgNum = 1;
+      while(params.length) {
+        var p = params.shift();
+        var msgNumAfter = false;
+        if(p[0] == '"') {
+          p = p.substr(1);
+        } else if(p[p.length-1] == '"') {
+          p = p.substr(0, p.length-1);
+          msgNumAfter = true;
+        }
+
+        if(msgNum == 1) msg1 += p + " ";
+        else msg2 += p + " ";
+
+        if(msgNumAfter) msgNum++;
+      }
+    } else {
+      params.forEach(function(el, inx){
+        if(inx <= 4)
+          msg1 += el + ' ';
+        else
+          msg2 += el + ' ';
+      });
+    }
+
+    msg1 = msg1.trim();
+    msg2 = msg2.trim();
+
     var url = 'http://version1.api.memegenerator.net/Instance_Create?username=w3bt3chirc&password=W3bT3ch1Rc507&languageCode=en&generatorID=740857&imageID=3459374&text0=' + msg1 + '&text1=' + msg2;
 
     var http = require('http');
