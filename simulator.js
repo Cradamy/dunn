8e2b75fbcc4c4d2306eb4b15f406bf878db528d1
Draceros <gr33nmous3@gmail.com>
2013-02-04 07:01:41 +0000

    HOLY FUCKING SHIT I ACTUALLY MADE A SIMULATOR

--- simulator.js
+++ simulator.js
@@ -14,7 +14,7 @@
 
 var program = require("commander"),
   fs = require("fs"),
-  sys = require("sys"),
+  sys = require("util"),
   events = require("events");
 
 program.version("1.0.0").
@@ -24,71 +24,118 @@ program.version("1.0.0").
     return value.split(",");
   }).
   option("-d, --directory", "Plugin directory").
-  option("-c, --command", "Plugin command").
+  option("-c, --cmd", "Plugin command").
   option("-D, --db", "Database name").
   parse(process.argv);
 
 console.log("Initializing Sandbunn");
-var irc = {
-  host: "sandbox",
-  port: 6667,
-  nick: "Sandbunn",
-  username: "SandBunn",
-  realname: "Dunn, in the dunes, making sandcastles",
-  command: program.command || ".",
-  alias: "Dunn",
-  database: program.db || "dunn",
-  admins: ["shirokuma", "penguin", "panda"],
-  userChannels: ["#sandbox", "#webtech"],
-  config: {},
-  hooks: [],
-  triggers: [],
-  plugins: [],
-  messagehandlers: [],
-  replies: [],
-  connection: null,
-  buffer: "",
-  encoding: "utf8",
-  timeout: 60*60*1000,
-  heap: []
+
+var ircServer = function(program) {
+  this.init(program);
+};
+
+sys.inherits(ircServer, events.EventEmitter);
+
+ircServer.prototype.init = function(program) {
+  this.host = "sandbox";
+  this.port = 6667;
+  this.nick = "Sandbunn";
+  this.username = "SandBunn";
+  this.realname = "Dunn, in the dunes, making sandcastles";
+  this.command = program.cmd || ".";
+  this.alias = "Dunn";
+  this.database = program.db || "dunn";
+  this.admins = ["shirokuma", "penguin", "panda"];
+  this.userChannels = ["#sandbox", "#webtech"];
+  this.config = {};
+  this.hooks = [];
+  this.triggers = [];
+  this.plugins = [];
+  this.messagehandlers = [];
+  this.replies = [];
+  this.connection = null;
+  this.buffer = "";
+  this.encoding = "utf8";
+  this.timeout = 60*60*1000;
+  this.heap = [];
 }
-sys.inherits(irc, events.EventEmmiter);
 
-console.log("Loading plugins");
-if(program.load) {
-  var xPlugins = fs.readdirSync(__dirname + "/plugins/");
-  while(xPlugins.length) {
-    var xPlugin = xPlugins.shift(),
-      ext = xPlugin.split("."); ext = ext[ext.length-1];
+var sandbunn = {
+  user: "nobody",
+  channel: null,
 
-    if(ext.toLowerCase() == "js") {
-      if(program.exclude && program.exclude.indexOf(xPlugin) == -1) {
-        var xP = require(__dirname + "/plugins/" + xPlugin), name = xPlugin.replace(".js", "");
-        irc.plugins[name] = xP;
+}
 
-        ['connect', 'data', 'numeric', 'message', 'join', 'part', 'quit', 'nick', 'privateMessage'].forEach(function(event) {
-          var onEvent = 'on' + event.charAt(0).toUpperCase() + event.substr(1),
-          callback = this.plugins[name][onEvent];
+ircServer.prototype.user = function(/**/) {
+  return sandbunn.user;
+}
 
-          if (typeof callback == 'function') {
-            this.addPluginListener(name, event, callback);
-          }
-        }, irc);
-      }
+ircServer.prototype.onMessage = function(msg) {
+
+  var msgs = msg.split(" ");
+  var argm = {
+    prefix: "",
+    command: "",
+    arguments: [],
+    lastarg: "",
+    orig: ""
+  };
+  argm.arguments.push(sandbunn.channel);
+  while(msgs.length) argm.arguments.push(msgs.shift());
+  msgs = msg.split(" ");
+
+
+
+  if(msgs[0][0] == "#") {
+    if(msgs[0][1] == ".") {
+        var cmd = msgs[0].substr(2);
+        switch(cmd) {
+          case "chusr": 
+            sandbunn.user = msgs[1];
+            this.send(sandbunn.channel, "Changed user", sandbunn.user)
+          break;
+
+          case "chchnl":
+            sandbunn.channel = "#" + (msgs[1].replace("#", ""));
+            this.send(sandbunn.channel, "Changed channel", sandbunn.user)
+          break;
+
+          case "this":
+            console.log(this);
+          break;
+
+          case "exit":
+            process.exit(0);
+          break;
+        }
+    } else if(msgs[0][1] == "!") {
+      this.emit(msgs[0].substr(2), msgs.splice(1).join(" "));
     }
+  } else if(msgs[0].indexOf(this.command) == 0) {
+    var trigger = msgs[0].replace(this.command, "");
+
+    if(typeof this.triggers[trigger] != "undefined") {
+      var trig = this.triggers[trigger];
+
+      if(trig.admin) {
+        if(this.admins.indexOf(sandbunn.user) == -1) {
+          this.send(sandbunn.channel, sandbunn.user + ": Insufficient permissions");
+          return false;
+        }
+      }
+
+      trig.callback.apply(this.plugins[trig.plugin], [this, sandbunn.channel, sandbunn.user, msgs.splice(1), argm, msg]); 
+    } //no msghandler as of now
   }
+
+  this.emit("message", argm);
 }
 
-irc.onMessage = function(msg) {
-  var target = msg.arguments[0],
-    nick = (this.user(msg.prefix) || '').toLowerCase(),
-    user = sandbunn.user,
-    m,
-    command = msg.command,
-    users = sandbunn.users;
+ircServer.prototype.send = function(t, m, u) {
+  console.log((u || this.nick) + "@" + t + " >>> " + m);
 }
 
-irc.addPluginListener = function (plugin, ev, f) {
+ircServer.prototype.addPluginListener = function (plugin, ev, f) {
   if(typeof this.hooks[plugin] == 'undefined') {
     this.hooks[plugin] = [];
   }
@@ -105,3 +152,71 @@ irc.addPluginListener = function (plugin, ev, f) {
 
   return this.on(ev, callback);
 };
+
+
+
+ircServer.prototype.addTrigger = function(trigger, callback, admin) {
+  if(typeof this.triggers[trigger] == 'undefined') {
+    if(typeof admin == "undefined") admin = 0;
+    admin = parseInt(admin);
+    this.triggers[trigger] = {plugin: trigger, callback: callback, admin: admin};
+  }
+};
+
+ircServer.prototype.addMessageHandler = function(trigger, callback) {
+  if(typeof this.messagehandlers[trigger] == 'undefined') {
+    this.messagehandlers[trigger] = {plugin: trigger, callback: callback};
+  }
+};
+
+
+ircServer.prototype.load = function(name) {
+  var xP = require(__dirname + "/plugins/" + name), name = name.replace(".js", "");
+  irc.plugins[name] = new xP.Plugin(this);
+
+  ['connect', 'data', 'numeric', 'message', 'join', 'part', 'quit', 'nick', 'privateMessage'].forEach(function(event) {
+    var onEvent = 'on' + event.charAt(0).toUpperCase() + event.substr(1),
+    callback = this.plugins[name][onEvent];
+
+    if (typeof callback == 'function') {
+      this.addPluginListener(name, event, callback);
+    }
+  }, irc);
+}
+
+var irc = new ircServer(program);
+sandbunn.user = irc.admins[0] || "shirokuma";
+sandbunn.channel = irc.userChannels[0] || "ohshitiforgottoaddachannel";
+
+console.log("Loading plugins");
+if(program.load) {
+  var xPlugins = fs.readdirSync(__dirname + "/plugins/");
+  while(xPlugins.length) {
+    var xPlugin = xPlugins.shift(),
+      ext = xPlugin.split("."); ext = ext[ext.length-1];
+
+    if(ext.toLowerCase() == "js") {
+      if(typeof program.exclude != "undefined" && program.exclude.indexOf(xPlugin) > -1) {
+        continue;
+      }
+      console.log("  ->  " + xPlugin.replace(".js", ""));
+      irc.load(xPlugin);
+    }
+  }
+} else {
+  while(program.args.length) {
+    var name = program.args.shift();
+    console.log("  ->  " + name);
+    irc.load(name + ".js");
+  }
+}
+
+sandbunn.requestInput = function() {
+  program.prompt(sandbunn.user + "@" + sandbunn.channel + ": ", function(v) {
+    irc.onMessage(v)
+    sandbunn.requestInput();
+  });
+}
+
+console.log("Root commands are #.chusr - change user, #.chchnl - change channel, #.this - prints this, #.exit - exits, #[EVENT] emits an event with msg");
+sandbunn.requestInput();
