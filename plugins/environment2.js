0e59d1cc3130a92d041353896f112962da70f800
Draceros <gr33nmous3@gmail.com>
2013-02-04 19:28:51 +0000

    I have no idea why he did so complicated with the environment.js plugin

---
+++ plugins/environment2.js
@@ -0,0 +1,33 @@
+/*
+ * @Plugin        Environment v2
+ * @Description   Plugins, triggers, message handlers etc.
+ * @Trigger       .env
+ * @Author        polar-bear
+ * @Copyright     Public Domain
+ *
+ */
+
+var Plugin = exports.Plugin = function(irc) {
+  irc.addTrigger("env", this.env)
+};
+
+Plugin.prototype.env = function(irc, channel, user, params, message) {
+  if(typeof message.arguments[2] == "undefined") return irc.send(channel, "Usage: "+irc.command+"env <plugins/triggers/handlers>");
+
+
+  switch(true) {
+    default:
+      irc.send(channel, "Usage: "+irc.command+"env <plugins/triggers/handlers>");
+    break
+    case message.arguments[2].indexOf("plugins") > -1:
+      irc.send(channel, "Loaded plugins are: " + Object.keys(irc.plugins).join(", "));
+    break;
+    case message.arguments[2].indexOf("triggers") > -1:
+      irc.send(channel, "Loaded triggers are: " + irc.command + Object.keys(irc.triggers).join(", " + irc.command));
+    break;
+    case message.arguments[2].indexOf("handlers") > -1:
+    console.log(irc);
+      irc.send(channel, "Loaded message handlers are: " + Object.keys(irc.messagehandlers).join(", "));
+    break;
+  }
+}
