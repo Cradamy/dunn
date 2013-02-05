2a69192aa20ad9672e04de698fc41bacceb700eb
Carlos Cardona <cgcardona@gmail.com>
2013-02-04 21:09:44 -0800

    fix path

--- plugins/dunnbook.js
+++ plugins/dunnbook.js
@@ -19,5 +19,5 @@ Plugin.prototype.dunnbook = function (irc, channel, nick, params, message, raw) 
   else if(params[0] == 'ch2')
     path = 'book_of_dunn_ch2';
   
-  irc.send(channel, nick + ': http://github.com/cgcardona/dunn/blob/master/docs/' + path + '.md');
+  irc.send(channel, nick + ': http://github.com/killswitch/dunn/blob/master/docs/' + path + '.md');
 };
