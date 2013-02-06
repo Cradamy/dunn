ee7a3d6b28b694960b815d8652650f3dc6400da0
Draceros <gr33nmous3@gmail.com>
2013-02-06 22:06:45 +0000

    Listable and _ in usernames

--- plugins/cleverdunn.js
+++ plugins/cleverdunn.js
@@ -35,12 +35,12 @@ Plugin = exports.Plugin = function(irc) {
 }
 
 Plugin.prototype.request = function(req, res) {
-	var sessionid = req.url.split("?")[0].substr(1);
+	var sessionid = decodeURIComponent(req.url.split("?")[0].substr(1));
 
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write("<!DOCTYPE html>\n<html><head><title>Cleverdunn</title><style>h1,h4{margin:0}body{font-family:Arial,sans-serif;}body,html,table,tr{width:100%}td{width:20%}td:nth-child(2){width:60%}tr{background: #eee}tr:first-child{background:#444;color: white;font-size: 1.5em}tr:nth-child(2n){background: #aaa}</style></head><body><h4>Cleverdunn.</h4>");
+	res.write("<!DOCTYPE html>\n<html><head><title>Cleverdunn</title><style>h1,h4{margin:0}a,a:hover{color: #444; text-decoration: underline;}body{font-family:Arial,sans-serif;}body,html,table,tr{width:100%}td{width:20%}td:nth-child(2){width:60%}tr{background: #eee}tr:first-child{background:#444;color: white;font-size: 1.5em}tr:nth-child(2n){background: #aaa}</style></head><body><h4><a href=\"/\">Cleverdunn.</a></h4>");
 	if(typeof Bots[sessionid] != "undefined") {
-		res.write("<h1>" + sessionid.split("_")[1] + " on #" + sessionid.split("_")[0] + "</h1><table><tr><td>Name</td><td>Message</td><td>Time</td></tr>");
+		res.write("<h1>" + sessionid.split(" ")[1] + " on #" + sessionid.split(" ")[0] + "</h1><table><tr><td>Name</td><td>Message</td><td>Time</td></tr>");
 
 		Bots[sessionid].log.forEach(function(log) {
 			res.write("<tr><td>"+log[0]+"</td><td>"+log[2]+"</td><td>"+log[1].toUTCString()+"</td></tr>");
@@ -48,24 +48,32 @@ Plugin.prototype.request = function(req, res) {
 
 		res.write("</table>");
 	} else { 
-		res.write("<h1>User/bot not found</h1>");
+		// res.write("<h1>User/bot not found</h1>");
+		res.write("<h1>All logs</h1><table><tr><td>Channel</td><td>Name</td>");
+
+		Object.keys(Bots).forEach(function(boto) {
+			bot = boto.split(" ");
+			res.write("<tr><td>"+bot[0]+"</td><td><a href=\""+boto+"\">"+bot[1]+"</a></td></tr>");
+		});
+
+		res.write("</table>");
 	}
 	res.write("</body></html>");
 	res.end();
 }
 
 Plugin.prototype.getLog = function(irc, channel, nick, match, message, raw) {
-	if(typeof Bots[channel.replace("#", "")+"_"+nick] != "undefined") {
+	if(typeof Bots[channel.replace("#", "")+" "+nick] != "undefined") {
 		// irc.send(channel, "Cleverbot log url: http://cleverbot.com/" + Bots[nick].params.logurl);
 		console.log(server.address());
-		irc.send(channel, "Log url: http://"+address+":"+server.address().port+"/"+channel.replace("#", "")+"_"+nick);
+		irc.send(channel, "Log url: http://"+address+":"+server.address().port+"/"+encodeURIComponent(channel.replace("#", ""))+" "+nick);
 	} else {
 		irc.send(channel, "Sorry, "+nick+" you don't have a cleverbot session yet.");
 	}
 }
 
 Plugin.prototype.debug = function(irc, channel, nick, match, message, raw) {
-	if(typeof Bots[channel.replace("#", "")+"_"+nick] != "undefined") {
+	if(typeof Bots[channel.replace("#", "")+" "+nick] != "undefined") {
 		irc.send(nick, JSON.stringify(Bots[nick].params));
 	} else {
 		irc.send(channel, "Sorry, "+nick+" you don't have a cleverbot session yet.");
@@ -73,7 +81,7 @@ Plugin.prototype.debug = function(irc, channel, nick, match, message, raw) {
 }
 
 Plugin.prototype.run = function(irc, channel, nick, match, message, raw) {
-	sessionid = channel.replace("#", "")+"_"+nick
+	sessionid = channel.replace("#", "")+" "+nick
 
 	if(typeof Bots[sessionid] == "undefined") {
 		Bots[sessionid] = new Cleverbot();
