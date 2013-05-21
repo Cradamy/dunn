45c76e964e2452b17a5661d65e73d1abc7aafda7
Josh Manders <josh@joshmanders.com>
2013-05-21 17:59:35 -0500

    Added messages plugin

---
+++ plugins/message.js
@@ -0,0 +1,20 @@
+/*
+* @Plugin Message
+* @Description Just a test to see if db stuff works.
+* @Trigger msg
+*
+* @Author Killswitch (Josh Manders)
+* @Website http://www.joshmanders.com
+* @Copyright Josh Manders 2013
+*
+*/
+
+Plugin = exports.Plugin = function (irc) {
+	irc.addTrigger('msg', this.messages);
+};
+
+Plugin.prototype.messages = function(irc, channel, nick, params, message, raw) {
+	this.db.query('SELECT say FROM messages WHERE message_id = 1 LIMIT 1', function (err, result) {
+		irc.send(channel, result[0].say);
+	});
+};
