18fef39aa684b9d042e2a60cde83553f6aeb99ea
Josh Manders <josh@joshmanders.com>
2013-05-21 18:47:26 -0500

    testing log

--- plugins/log.js
+++ plugins/log.js
@@ -14,7 +14,7 @@ Plugin = exports.Plugin = function (irc) {
 };
 
 Plugin.prototype.onMessage = function (msg) {
-	var nick user_nick = (this.irc.user(msg.prefix) || '').toLowerCase(),
+	var user_nick = (this.irc.user(msg.prefix) || '').toLowerCase(),
 		sql = {
 			created_on: Date.create(new Date()).format('{yyyy}-{dd}-{dd} {HH}:{mm}:{ss}'),
 			nick: user_nick,
