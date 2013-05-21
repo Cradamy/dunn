33b44250d7dec50fb54183b5dff33a5b348d2895
Josh Manders <josh@joshmanders.com>
2013-05-21 17:27:09 -0500

    fixing all the bullshit that broke it previously.

--- plugins/dunn.js
+++ plugins/dunn.js
@@ -1,47 +1,64 @@
 /*
- * @Plugin        Dunn
- * @Description   Random triggers based on things having to do with Dunn himself.
- * @Trigger       .about
- * @Trigger       .code
- *
- * @Author        Killswitch (Josh Manders)
- * @Website       http://www.joshmanders.com
- * @Copyright     Josh Manders 2012
- *
- */
+* @Plugin Dunn
+* @Description Random triggers based on things having to do with Dunn himself.
+* @Trigger .about
+* @Trigger .code
+*
+* @Author Killswitch (Josh Manders)
+* @Website http://www.joshmanders.com
+* @Copyright Josh Manders 2012
+*
+*/
 Plugin = exports.Plugin = function (irc) {
-  this.ircObj = irc;
-  botNick = this.ircObj.nick,
-  irc.addTrigger('about', this.about);
-  irc.addTrigger('code', this.code);
+	this.ircObj = irc;
+	irc.addTrigger('about', this.about);
+	irc.addTrigger('code', this.code);
+	irc.addTrigger('env', this.environment);
 };
 
 Plugin.prototype.onNumeric = function(irc) {
-  if (irc.command !== '376') {
-      return;
-  }
-  for (var i = 0; i < this.ircObj.userChannels.length; i++) {
-    var channelName = this.ircObj.userChannels[i], password;
-    if (typeof(channelName) == "object") {
-      password = channelName.password;
-      channelName = channelName.name;
-    }
-    var chan = new this.ircObj.channelObj(this.ircObj, channelName, true, password);
-    this.ircObj.channels[chan.name] = chan;
-  }
+	if (irc.command !== '376') {
+		return;
+	}
+	for (var i = 0; i < this.ircObj.userChannels.length; i++) {
+		var channelName = this.ircObj.userChannels[i], password;
+		if (typeof(channelName) == 'object') {
+			password = channelName.password;
+			channelName = channelName.name;
+		}
+		var chan = new this.ircObj.channelObj(this.ircObj, channelName, true, password);
+		this.ircObj.channels[chan.name] = chan;
+	}
 };
 
 Plugin.prototype.onMessage = function (irc) {
-  if (irc.arguments[1].match(/dunn dunn/i))
-  {
-      this.ircObj.send(irc.arguments[0], 'dunnnnnnnnnn');
-  }
+	if (irc.arguments[1].match(/^dunn dunn$/i))
+	{
+		this.ircObj.send(irc.arguments[0], 'dunnnnnnnnnn');
+	}
 };
 
 Plugin.prototype.about = function (irc, channel, nick, params, message, raw) {
-  irc.send(channel, nick + ': My name is ' + botNick + ', I am written in Node.js and utilize MongoDB as my data storage. I was written by Killswitch.');
+	irc.send(channel, nick + ': My name is ' + this.ircObj.nick + ', I am written in Node.js and utilize MongoDB as my data storage. I was written by Killswitch.');
 };
 
 Plugin.prototype.code = function (irc, channel, nick, params, message, raw) {
-  irc.send(channel, nick + ': You can view, and fork me in my code hole to contribute on GitHub @ http://www.github.com/killswitch/dunn.');
+	irc.send(channel, nick + ': You can view, and fork me in my code hole to contribute on GitHub @ http://www.github.com/webtechirc/dunn.');
+};
+
+Plugin.prototype.environment = function(irc, channel, nick, params, message, raw) {
+	if(typeof params[0] == 'undefined') {
+		return irc.send(channel, 'Usage: ' + irc.command + 'env <plugins/triggers>');
+	}
+	switch(true) {
+		default:
+		irc.send(channel, 'Usage: ' + irc.command + 'env <plugins/triggers/handlers>');
+		break
+		case params[0].indexOf('plugins') > -1:
+		irc.send(channel, 'Loaded plugins are: ' + Object.keys(irc.plugins).join(', '));
+		break;
+		case params[0].indexOf('triggers') > -1:
+		irc.send(channel, 'Loaded triggers are: ' + irc.command + Object.keys(irc.triggers).join(', ' + irc.command));
+		break;
+	}
 };
