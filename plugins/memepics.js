a6b6f8f8bd768a653d763aa5dfcd403c62b71ad1
Josh Manders <josh@joshmanders.com>
2013-05-21 20:31:56 -0500

    fix memepics

--- plugins/memepics.js
+++ plugins/memepics.js
@@ -28,6 +28,7 @@ var memes = {
 };
 
 Plugin = exports.Plugin = function (irc) {
+	var this.memeTriggers = [];
 	var self = this;
 	irc.addTrigger('meme', this.usage);
 	irc.addTrigger('memetriggers', this.triggers);
