107f51b3ffd98205eda4ddb9583b3ab5be17297e
Josh Manders <josh@joshmanders.com>
2013-05-21 19:44:15 -0500

    weather

--- plugins/weather.js
+++ plugins/weather.js
@@ -15,6 +15,13 @@ Plugin = exports.Plugin = function (irc) {
 Plugin.prototype.weather = function (irc, channel, nick, params, message) {
     irc.httpGet('http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(params[0]) + '&units=imperial', function (err, res, result) {
 		var forcast = JSON.parse(result);
-		irc.send(channel, nick + ': ' + forcast.name + ': ' + forcast.weather[0].main + ', ' + forcast.main.temp + 'F (High: ' + forcast.main.temp_max + 'F - Low: ' + forcast.main.temp_min + 'F), Humidity ' + forcast.main.humidity + '%, Wind: ' + forcast.wind.speed + 'MPH');
+		if (forcast.message.length < 1)
+		{
+			irc.send(channel, nick + ': ' + forcast.name + ': ' + forcast.weather[0].main + ', ' + forcast.main.temp + 'F (High: ' + forcast.main.temp_max + 'F - Low: ' + forcast.main.temp_min + 'F), Humidity ' + forcast.main.humidity + '%, Wind: ' + forcast.wind.speed + 'MPH');
+		}
+		else
+		{
+			irc.send(channel, nick + ': Sorry I could not find weather for your request, try something else like: London, Chicago, New York City.');
+		}
 	});
 };
