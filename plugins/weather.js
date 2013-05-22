35ec010baa7cfb32883d32bcae73962daf4dcc28
Josh Manders <josh@joshmanders.com>
2013-05-21 19:44:56 -0500

    weather

--- plugins/weather.js
+++ plugins/weather.js
@@ -15,7 +15,7 @@ Plugin = exports.Plugin = function (irc) {
 Plugin.prototype.weather = function (irc, channel, nick, params, message) {
     irc.httpGet('http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(params[0]) + '&units=imperial', function (err, res, result) {
 		var forcast = JSON.parse(result);
-		if (forcast.message.length < 1)
+		if (!forcast.message)
 		{
 			irc.send(channel, nick + ': ' + forcast.name + ': ' + forcast.weather[0].main + ', ' + forcast.main.temp + 'F (High: ' + forcast.main.temp_max + 'F - Low: ' + forcast.main.temp_min + 'F), Humidity ' + forcast.main.humidity + '%, Wind: ' + forcast.wind.speed + 'MPH');
 		}
