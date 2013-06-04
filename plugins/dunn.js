1b051ba7c0fa6d771a5b80fb144a8c976a50f7cb
Josh Manders <josh@joshmanders.com>
2013-06-03 21:49:56 -0500

    added ops topic change ability

--- plugins/dunn.js
+++ plugins/dunn.js
@@ -97,6 +97,6 @@ Plugin.prototype.restart = function(irc, channel, nick, params, message, raw) {
 Plugin.prototype.topic = function(irc, channel, nick, params, message, raw) {
 	var topic = 'Welcome to #webtech - Certified Web Ninjas || {topic} || Pastebin: http://refheap.com || JavaScript: http://jsfiddle.net || Github: https://github.com/webtechirc || Promo Code "SSDTWEET" for $10 credit at digitalocean.com';
 	console.log(topic);
-	console.log(topic.replace('{topic}', message));
+	console.log(topic.replace('{topic}', params.join(' ')));
 	irc.raw('TOPIC', channel + ' ' + topic);
 };
