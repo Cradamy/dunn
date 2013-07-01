5c4271929bd77a300356186923ce9130223f5775
Josh Manders <josh@joshmanders.com>
2013-07-01 15:42:03 -0500

    change digitalocean promocode in default topic

--- plugins/dunn.js
+++ plugins/dunn.js
@@ -98,7 +98,7 @@ Plugin.prototype.restart = function(irc, channel, nick, params, message, raw) {
 };
 
 Plugin.prototype.topic = function(irc, channel, nick, params, message, raw) {
-	var topic = 'Welcome to #webtech - Certified Web Ninjas || {topic} || Pastebin: http://refheap.com || JavaScript: http://jsfiddle.net || Github: https://github.com/webtechirc || Promo Code "SSDTWEET" for $10 credit at digitalocean.com';
+	var topic = 'Welcome to #webtech - Certified Web Ninjas || {topic} || Pastebin: http://refheap.com || JavaScript: http://jsfiddle.net || Github: https://github.com/webtechirc || Promo Code "SSDFUEL" for $10 credit at digitalocean.com';
 	irc.raw('TOPIC', channel, ':' + topic.replace('{topic}', params.join(' ')));
 };
 
